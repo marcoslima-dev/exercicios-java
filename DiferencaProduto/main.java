0b337504bbcc5fa6f5324c130d16988a08a7faa7
Marcos Lima <marcoslima0511@outlook.com>
2022-09-15 00:46:59 -0300

    second commit

---
+++ DiferencaProduto/main.java
@@ -0,0 +1,17 @@
+package DiferencaProduto;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A,B,C,D,diferenca;
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+        D = sc.nextInt();
+        diferenca = (A * B - C * D);
+        System.out.println("DIFERENCA: "+ diferenca);
+        sc.close();
+    }
+}

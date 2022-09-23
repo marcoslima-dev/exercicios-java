fe260fddb2d8690658e7df8eeb5ef891b4fcbdb2
Marcos Lima <66645946+marcoslima-dev@users.noreply.github.com>
2022-09-23 20:09:42 -0300

    update question

--- ExercicioSoma/main.java
+++ ExercicioSoma/main.java
@@ -1,6 +1,8 @@
 package ExercicioSoma;
 
 import java.util.Scanner;
+/*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa, conforme exemplos.*/
 
 public class main {
     public static void main(String[] args) {

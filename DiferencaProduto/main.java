30cfae4be306667154a3f34381ef73efe01f6988
Marcos Lima <66645946+marcoslima-dev@users.noreply.github.com>
2022-09-23 20:09:03 -0300

    update question

--- DiferencaProduto/main.java
+++ DiferencaProduto/main.java
@@ -1,7 +1,8 @@
 package DiferencaProduto;
 
 import java.util.Scanner;
-
+/*Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
+de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).*/
 public class main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
